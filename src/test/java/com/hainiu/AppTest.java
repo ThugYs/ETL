a085255cb53eb0f7b4923587c771283c7c6f2147
qcy961011 <18568437879@163.com>
2019-04-23 21:03:22 +0800

    inti

---
+++ src/test/java/com/hainiu/AppTest.java
@@ -0,0 +1,23 @@
+package com.hainiu;
+
+import static org.junit.Assert.assertTrue;
+
+import com.hainiu.util.IPParser;
+import com.hainiu.util.LogParser;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        IPParser.RegionInfo regionInfo = IPParser.getInstance().analyseIp("120.196.100.99");
+        System.out.println(regionInfo.getProvince());
+    }
+}
